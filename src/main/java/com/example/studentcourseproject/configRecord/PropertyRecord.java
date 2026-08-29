1e6f43142141551ade63bf5251bc0a3ea7d82c5d
MIRSHAHAKHAYEV <agayev.mirshah.yusif.2022@asoiu.edu.az>
2026-08-29 20:31:19 +0400

    ConfigRecord validated

--- src/main/java/com/example/studentcourseproject/configRecord/PropertyRecord.java
+++ src/main/java/com/example/studentcourseproject/configRecord/PropertyRecord.java
@@ -1,14 +1,19 @@
 package com.example.studentcourseproject.configRecord;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 @ConfigurationProperties(prefix = "app")
+@Validated
 public record PropertyRecord(
-        String name,
-        int maximumStudent,
-        String courseDefaultDuration,
+        @NotBlank String name,
+        @Positive int maximumStudent,
+        @Min(1) long courseDefaultDuration,
         boolean isEnabled,
         List<String> allowedCountries) {
 }
